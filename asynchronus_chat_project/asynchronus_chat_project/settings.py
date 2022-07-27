b356735d3ba16de8327098bcd2cc3bc4e21f9558
Farhan Tahmid <farhantahmid881@gmail.com>
2022-07-27 22:00:23 +0600

    django env variable

--- asynchronus_chat_project/asynchronus_chat_project/settings.py
+++ asynchronus_chat_project/asynchronus_chat_project/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+#Setting up environment variable
+import environ
+env=environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-off#6ffl*upvl3n*7pt5qf_llqc0&6a9q$nync_dp)-p*$w9q8'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
