dba4fea1815093dd12b5b19d6d0709308baca58d
Farhan Tahmid <farhantahmid881@gmail.com>
2022-07-29 12:45:12 +0600

    problem with receiving messages in websocket

--- asynchronus_chat_project/chat_app/consumers.py
+++ asynchronus_chat_project/chat_app/consumers.py
@@ -1,3 +1,4 @@
+from email import message
 from time import sleep
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -30,4 +31,21 @@ class ChatRoomConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+        
+    async def receive(self,text_data):
+        text_data_json=json.loads(text_data)
+        message=text_data_json['message']
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type':'chat_message',
+                'message':message,
+            }
+        )
+    async def chatroom_message(self,event):
+        message=event['message']
+        await self.send(text_data=json.dumps({
+            'message':message,
+        }))
+
     pass
